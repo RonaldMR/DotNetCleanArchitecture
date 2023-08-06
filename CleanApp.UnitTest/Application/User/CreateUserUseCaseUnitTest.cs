9cfec67c808916d94e8a709accdf3b492efcca9f
Ronald Marrou <ronaldmarrou@gmail.com>
2023-08-06 03:55:41 -0500

    Refactoring Unit Tests, splitting

--- CleanApp.UnitTest/Application/User/CreateUserUseCaseUnitTest.cs
+++ CleanApp.UnitTest/Application/User/CreateUserUseCaseUnitTest.cs
@@ -7,7 +7,7 @@ using Moq;
 
 namespace CleanApp.UnitTest
 {
-    public class UserUnitTest
+    public class CreateUserUseCaseUnitTest
     {
         [Fact]
         public async void ShouldNotCreateUserWhenEmailExists()
@@ -61,57 +61,6 @@ namespace CleanApp.UnitTest
             Assert.Equal(result.LastName, expectedUser.LastName);
             Assert.Equal(result.EmailAddress, expectedUser.EmailAddress);
             Assert.Equal(result.Password, expectedUser.Password);
-        }
-
-        [Fact]
-        public async void ShouldNotLoginWhenUserIsNotValid()
-        {
-            var userRepositoryMock = new Mock<IUserRepository>();
-
-            userRepositoryMock.Setup(repository => repository.Get(It.IsAny<string>())).Returns(Task.FromResult<UserEntity>(null));
-
-            var useCase = new LogInUserUseCase(userRepositoryMock.Object);
-
-            var act = () => useCase.Execute("FOO", "FOO");
-
-            await Assert.ThrowsAsync<AccessDeniedException>(act);
-        }
-
-        [Fact]
-        public async void ShouldNotLoginWhenUserIsValidAndPasswordIsNotValid()
-        {
-            var userRepositoryMock = new Mock<IUserRepository>();
-
-            var userMock = new UserEntity(Guid.NewGuid(), "FOO", "FOO", "FOO", "FOOPASSWORD", DateTime.Now);
-
-            userRepositoryMock.Setup(repository => repository.Get(It.IsAny<string>())).Returns(Task.FromResult<UserEntity>(userMock));
-
-            var useCase = new LogInUserUseCase(userRepositoryMock.Object);
-
-            var act = () => useCase.Execute("FOO", "ANOTHERFOOPASSWORD");
-
-            await Assert.ThrowsAsync<AccessDeniedException>(act);
-        }
-
-        [Fact]
-        public async void ShouldLoginWhenUserAndPasswordAreValid()
-        {
-            var userRepositoryMock = new Mock<IUserRepository>();
-
-            var userMock = new UserEntity(Guid.NewGuid(), "FOO", "FOO", "FOO", "FOOPASSWORD", DateTime.Now);
-
-            userRepositoryMock.Setup(repository => repository.Get(It.IsAny<string>())).Returns(Task.FromResult<UserEntity>(userMock));
-
-            var useCase = new LogInUserUseCase(userRepositoryMock.Object);
-
-            var act = () => useCase.Execute("FOO", "FOOPASSWORD");
-
-            var result = await act();
-
-            Assert.Equal(result.FirstName, userMock.FirstName);
-            Assert.Equal(result.LastName, userMock.LastName);
-            Assert.Equal(result.EmailAddress, userMock.EmailAddress);
-            Assert.Equal(result.Password, userMock.Password);
         }
     }
 }
---
+++ CleanApp.UnitTest/Application/User/LoginUserUseCaseUnitTest.cs
@@ -0,0 +1,62 @@
+﻿using CleanApp.Application.Exceptions;
+using CleanApp.Application.UseCases.User;
+using CleanApp.Domain.Entities;
+using CleanApp.Domain.Repositories;
+using Moq;
+
+namespace CleanApp.UnitTest
+{
+    public class UserUnitTest
+    {
+        [Fact]
+        public async void ShouldNotLoginWhenUserIsNotValid()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+
+            userRepositoryMock.Setup(repository => repository.Get(It.IsAny<string>())).Returns(Task.FromResult<UserEntity>(null));
+
+            var useCase = new LogInUserUseCase(userRepositoryMock.Object);
+
+            var act = () => useCase.Execute("FOO", "FOO");
+
+            await Assert.ThrowsAsync<AccessDeniedException>(act);
+        }
+
+        [Fact]
+        public async void ShouldNotLoginWhenUserIsValidAndPasswordIsNotValid()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+
+            var userMock = new UserEntity(Guid.NewGuid(), "FOO", "FOO", "FOO", "FOOPASSWORD", DateTime.Now);
+
+            userRepositoryMock.Setup(repository => repository.Get(It.IsAny<string>())).Returns(Task.FromResult<UserEntity>(userMock));
+
+            var useCase = new LogInUserUseCase(userRepositoryMock.Object);
+
+            var act = () => useCase.Execute("FOO", "ANOTHERFOOPASSWORD");
+
+            await Assert.ThrowsAsync<AccessDeniedException>(act);
+        }
+
+        [Fact]
+        public async void ShouldLoginWhenUserAndPasswordAreValid()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+
+            var userMock = new UserEntity(Guid.NewGuid(), "FOO", "FOO", "FOO", "FOOPASSWORD", DateTime.Now);
+
+            userRepositoryMock.Setup(repository => repository.Get(It.IsAny<string>())).Returns(Task.FromResult<UserEntity>(userMock));
+
+            var useCase = new LogInUserUseCase(userRepositoryMock.Object);
+
+            var act = () => useCase.Execute("FOO", "FOOPASSWORD");
+
+            var result = await act();
+
+            Assert.Equal(result.FirstName, userMock.FirstName);
+            Assert.Equal(result.LastName, userMock.LastName);
+            Assert.Equal(result.EmailAddress, userMock.EmailAddress);
+            Assert.Equal(result.Password, userMock.Password);
+        }
+    }
+}
